6a905a8fa1f8583d9d1d3cff15cd4c54bf816e65
Naufal Fadhiil <naufalfadhiil2206@gmail.com>
2026-07-15 16:14:36 +0700

    chore(backend): add cors, helmet, morgan

--- apps/backend/src/app.ts
+++ apps/backend/src/app.ts
@@ -1,7 +1,17 @@
 import express, { Request, Response, NextFunction } from "express";
+import cors from "cors";
+import helmet from "helmet";
+import morgan from "morgan";
+
 import { publicRouter, protectedRouter } from "./routes/index.js";
 
 const app = express();
+
+app.use(helmet());
+
+app.use(cors());
+
+app.use(morgan("dev"));
 
 app.use(express.json());
 
@@ -16,13 +26,22 @@ app.use((req: Request, res: Response) => {
   });
 });
 
-app.use((err: unknown, req: Request, res: Response, next: NextFunction) => {
-  console.error("Unhandled error:", err);
-  res.status(500).json({
-    success: false,
-    message: "Terjadi kesalahan pada server.",
-    errors: {},
-  });
-});
+/* Global Error Handler */
+app.use(
+  (
+    err: unknown,
+    req: Request,
+    res: Response,
+    next: NextFunction
+  ) => {
+    console.error(err);
+
+    res.status(500).json({
+      success: false,
+      message: "Terjadi kesalahan pada server.",
+      errors: {},
+    });
+  }
+);
 
 export default app;
