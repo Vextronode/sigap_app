fcdc7e63584e5f00bbd63db0faf772f1ff8bb5a3
Naufal Fadhiil <naufalfadhiil2206@gmail.com>
2026-07-16 21:38:28 +0700

    Implementasi Backend-API: Auth dan Weather (#17)
    * feat(backend): setup Prisma schema and initial database migration
    
    * feat(api-backend): API implement authentication and  weather service

--- apps/backend/src/utils/jwt.util.ts
+++ apps/backend/src/utils/jwt.util.ts
@@ -1,5 +1,5 @@
 import jwt, { SignOptions } from "jsonwebtoken";
-import { JwtPayload } from "../types/auth.types.js";
+import { JwtPayload } from "../types/auth.type.js";
 
 const JWT_SECRET = process.env.JWT_SECRET;
 const JWT_EXPIRES_IN = process.env.JWT_EXPIRES_IN ?? "1d";
